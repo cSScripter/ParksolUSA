73a850bdf37aba31825209afa7f67f100a7c8000
cSScripter <parksolprojects@gmail.com>
2025-08-14 18:36:09 +0300

    font fix

--- src/components/aipgs.tsx
+++ src/components/aipgs.tsx
@@ -16,7 +16,7 @@ export default function Aipgs() {
 
   return (
     <section className="flex justify-center py-10 md:py-40">
-      <div className="flex flex-col md:flex-row items-center gap-16 w-full max-w-7xl px-4 md:px-0">
+      <div className="flex flex-col md:flex-row items-center gap-0 md:gap-16 w-full max-w-7xl px-6 md:px-0">
 
         {/* Swiper in left column */}
         <div className="w-full md:w-1/2 flex justify-center">
@@ -42,13 +42,13 @@ export default function Aipgs() {
         {/* Text in right column */}
         <div className="w-full md:w-1/2 flex flex-col justify-start items-start">
           <h2>AI Parking Guiding<br /> System</h2>
-          <p className="my-5">ParksolUSA video analytic space system includes:</p>
-          <ul className="list-disc my-1 ml-7 space-y-2">
+          <p className="my-5 pb-2 md:pb-0">ParksolUSA video analytic space system includes:</p>
+          <ul className="list-disc my-1np ml-7 space-y-2">
             <li>Introducing a new way to oversee parking - managing right from the parking space.</li>
             <li>AI surveillance of vehicles in parked spaces, nested zones, and during transit between levels or lots.</li>
             <li>AI monitoring of vehicles entering or leaving garages, different levels, and virtual gates.</li>
             <li>Achieving 100% accuracy in AI vehicle detection when the system is operational.</li>
-            <li>Prompting the AI with the right questions – shaping the evolution of parking alerts and events.</li>
+            <li>Prompting the AI with the right questions - shaping the evolution of parking alerts and events.</li>
           </ul>
         </div>
 
