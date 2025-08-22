dbb9f8665f92df344c4178802914cf70f8581b75
cSScripter <parksolprojects@gmail.com>
2025-08-22 14:57:48 +0300

    innovative solutions section complete

--- src/components/innovativesolutions.tsx
+++ src/components/innovativesolutions.tsx
@@ -1,12 +1,12 @@
 export default function InnovativeSolutions() {
   return (
-    <section className="flex flex-col items-center mt-5 md:mt-20">
+    <section className="flex flex-col items-center pt-5 md:pb-20 md:gap-5 md:pt-20 border-b border-gray-200">
       <h2>
         <span className="font-bold">Innovative</span> Solutions
       </h2>
 
-      <div className="flex flex-col md:flex-row items-center justify-center gap-5 md:gap-20 mt-5 md:mt-10">
-        <h3 className="px-5 md:pt-10">
+      <div className="flex w-full flex-col md:flex-row items-center justify-center gap-5 md:gap-10 mt-5 md:my-10">
+        <h3 className="md:pt-10 md:pb-10">
           We offer <span className="font-bold">more control</span> over your
           spaces - <br /> whether you have a multi-level parking <br /> garage,
           outdoor lot, or{" "}
@@ -15,8 +15,14 @@ export default function InnovativeSolutions() {
             between
           </span>
         </h3>
-        <h2>Video Goes Here!</h2>
-      </div>
+ 
+  <iframe
+    className="md:w-[29%] aspect-video border-0"
+    src="https://www.youtube.com/embed/yV5M6BCBhs0?si=sd2NBZB08kDPR_8h"
+    title="Parking Systems Overview Video"
+  />
+</div>
+      
     </section>
   );
 }
